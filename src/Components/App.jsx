ea83fa84cfe2a5e98989c3ba4dc65f6d4222188e
Einvy <JohnMichaelLudlow11@gmail.com>
2026-02-26 20:53:27 -0700

    working on making small changes

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -4,6 +4,7 @@ import Footer from "./Footer";
 import Body from "./Body";
 import Skills from "./Skills";
 import Projects from "./Projects";
+import Keyboard from "./Keyboard";
 function App() {
   return (
     <div>
@@ -11,6 +12,7 @@ function App() {
       <Body />
       <Skills />
       <Projects />
+      <Keyboard />
       <Footer />
     </div>
   );
---
+++ src/Components/Keyboard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Keyboard() {
+  return (
+    <section className="Keyboard" aria-labelledby="keyboard-heading">
+      <h2 id="keyboard-heading">Keyboard i use</h2>
+      <img src="/images/keyboard.jpg" alt="Keyboard" />
+    </section>
+  );
+}
+
+export default Keyboard;
